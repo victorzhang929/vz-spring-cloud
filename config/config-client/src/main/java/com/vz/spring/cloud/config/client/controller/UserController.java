5ecc085d46fd783125920cb7a4b082cabf962271
victorzhang <victorzhang0929@hotmail.com>
2018-08-31 11:58:25 +0800

    update: config配置env

--- config/config-client/src/main/java/com/vz/spring/cloud/config/client/controller/UserController.java
+++ config/config-client/src/main/java/com/vz/spring/cloud/config/client/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.vz.spring.cloud.config.client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @email victorzhang0929@hotmail.com
  */
 @RestController
+@RefreshScope
 public class UserController {
 
     @Value("${from}")
     private String from;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/user/from")
     public String getFrom() {
         return this.from;
+    }
+
+    @GetMapping("/env/from")
+    public String getEnvFrom() {
+        return this.environment.getProperty("from");
     }
 }
