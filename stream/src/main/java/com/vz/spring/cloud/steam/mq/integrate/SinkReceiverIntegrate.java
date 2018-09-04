5aeb41c7cf697e15ae8037436853751bc78645a1
victorzhang <victorzhang0929@hotmail.com>
2018-09-04 18:05:05 +0800

    fix: transformer函数使用

--- stream/src/main/java/com/vz/spring/cloud/steam/mq/integrate/SinkReceiverIntegrate.java
+++ stream/src/main/java/com/vz/spring/cloud/steam/mq/integrate/SinkReceiverIntegrate.java
@@ -4,19 +4,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author zhangwei
  * @date 04/09/2018 16:11
  * @email victorzhang0929@hotmail.com
  */
-@EnableBinding(value = SinkSenderIntegrate.class)
+@EnableBinding(value = SinkSenderIntegrate.SinkOutput.class)
 public class SinkReceiverIntegrate {
 
     private static final Logger logger = LoggerFactory.getLogger(SinkReceiverIntegrate.class);
 
     @ServiceActivator(inputChannel = SinkSenderIntegrate.SinkOutput.OUTPUT)
-    public void receive(String payload) {
-        logger.info("Received: {}", payload);
+    public void receive(String message) {
+        logger.info("Received: {}", message);
     }
+
+    @Transformer(inputChannel = SinkSenderIntegrate.SinkOutput.OUTPUT, outputChannel = SinkSenderIntegrate.SinkOutput.OUTPUT)
+    public String transform(Date message) {
+        return new SimpleDateFormat("yyyy-MM-dd").format(message);
+    }
+
 }
--- stream/src/main/java/com/vz/spring/cloud/steam/mq/integrate/SinkSenderIntegrate.java
+++ stream/src/main/java/com/vz/spring/cloud/steam/mq/integrate/SinkSenderIntegrate.java
@@ -5,12 +5,10 @@ import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
-import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -21,15 +19,10 @@ import java.util.Date;
 @EnableBinding(value = SinkSenderIntegrate.SinkOutput.class)
 public class SinkSenderIntegrate {
 
-    @Transformer(inputChannel = SinkOutput.OUTPUT, outputChannel = SinkOutput.OUTPUT)
-    public String transform(Date message) {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(message);
-    }
-
     @Bean
-    @InboundChannelAdapter(value =SinkOutput.OUTPUT, poller = @Poller(fixedDelay = "2000"))
-    public MessageSource<String> timeMessageSource() {
-        return () -> new GenericMessage<>(transform(new Date()));
+    @InboundChannelAdapter(value =SinkSenderIntegrate.SinkOutput.OUTPUT, poller = @Poller(fixedDelay = "2000"))
+    public MessageSource<Date> timeMessageSource() {
+        return () -> new GenericMessage<>(new Date());
     }
 
     public interface SinkOutput {
