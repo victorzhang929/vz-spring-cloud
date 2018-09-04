b997fe69639198e7f7163d914d3b801e04a5b9e5
victorzhang <victorzhang0929@hotmail.com>
2018-09-04 16:55:47 +0800

    update: transform格式调整

--- stream/src/main/java/com/vz/spring/cloud/steam/mq/integrate/SinkSenderIntegrate.java
+++ stream/src/main/java/com/vz/spring/cloud/steam/mq/integrate/SinkSenderIntegrate.java
@@ -5,10 +5,12 @@ import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -19,10 +21,15 @@ import java.util.Date;
 @EnableBinding(value = SinkSenderIntegrate.SinkOutput.class)
 public class SinkSenderIntegrate {
 
+    @Transformer(inputChannel = SinkOutput.OUTPUT, outputChannel = SinkOutput.OUTPUT)
+    public String transform(Date message) {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(message);
+    }
+
     @Bean
     @InboundChannelAdapter(value =SinkOutput.OUTPUT, poller = @Poller(fixedDelay = "2000"))
-    public MessageSource<Date> timeMessageSource() {
-        return () -> new GenericMessage<>(new Date());
+    public MessageSource<String> timeMessageSource() {
+        return () -> new GenericMessage<>(transform(new Date()));
     }
 
     public interface SinkOutput {
